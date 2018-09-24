f81b21711ade1530bc2c6bbecf4cceaf66d5f640
YellowNote <32231233+YellowNote@users.noreply.github.com>
2018-09-24 17:11:50 -0600

    Update Lab1.cpp

--- Homework/Lab1.cpp
+++ Homework/Lab1.cpp
@@ -16,7 +16,7 @@ using namespace std;
 int main() {
 	//initilization
 	char filename[50];
-	int temp, numcount = 0, counting, first = 5, second, last, secondlast;
+	int temp, numcount = 0, counting, first , second, last, secondlast;
 
 	cout << "Enter file to read from: " << endl;//getting name of file to open
 	cin.get(filename, 255);
@@ -28,30 +28,16 @@ int main() {
 		while (!filein.eof()) {//eof == end of file
 			if (filein >> temp) {//making sure it is an int
 				numcount++;
+				if(numcount == 1){
+					first = temp;//1 will be the first item
+				}else(numcount == 2){
+					second = temp;//2 will be the second item
+				}
+				seondlast = temp;//saves so when we get to the end temp will be last and secondlast will be the second to last
 			}
-		}
+		}//end while
 		counting = numcount;//setting for checks 
 		filein.close();
-		filein.open(filename);
-		if (filein.is_open()) {//making sure file is open
-			while (!filein.eof()) {
-				if (filein >> temp) {
-					counting--;//with one counting down we can compare to find first, second, last, and second to last
-					if (numcount - counting == 1) {//looking for first
-						first = temp;
-					}
-					if (numcount - counting == 2) {//looking for second
-						second = temp;
-					}
-					if (counting == 1) {//looking for second to last
-						secondlast = temp;
-					}
-					if (counting == 0) {//looking for last
-						last = temp;
-					}
-				}
-			}//output everything
-		}
 		cout << "Number of integers: " << numcount << endl
 			<< "First integer: " << first << endl
 			<< "Second integer: " << second << endl
